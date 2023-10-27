9c6a9192f44ee4b5f394f35c45c872ba937f77d9
Zainal Salamun <zainalrtf@gmail.com>
2023-10-27 17:04:55 +0700

    add gmap datasource

---
+++ lib/data/remote_datasources/GmapDatasource.dart
@@ -0,0 +1,109 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:geocoding/geocoding.dart' as geo;
+
+import '../models/gmap_model.dart';
+
+class GmapDatasource {
+  Future<bool> _getPermission(Location location) async {
+    late bool serviceEnabled;
+    late PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return false;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  Future<Either<String, GmapModel>> getCurrentPosition() async {
+    try {
+      final Location location = Location();
+      late LocationData locationData;
+
+      final state = await _getPermission(location);
+
+      if (!state) return const Left("Permission not granted");
+
+      locationData = await location.getLocation();
+      final latLng = LatLng(
+        locationData.latitude!,
+        locationData.longitude!,
+      );
+
+      final info = await geo.placemarkFromCoordinates(
+        latLng.latitude,
+        latLng.longitude,
+      );
+
+      String? address;
+
+      if (info.isNotEmpty) {
+        final place = info[0];
+        address =
+            '$place.street, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+      } else {
+        address = 'Location not found';
+      }
+
+      return Right(GmapModel(latLng: latLng, address: address));
+    } on PlatformException catch (e) {
+      if (e.code == 'IO_ERROR') {
+        debugPrint(
+            'A network error occurred trying to lookup the spplied coordinats : ${e.message}');
+      }
+
+      return Left(e.message!);
+    } catch (e) {
+      debugPrint('An unknown error occured : $e');
+      return Left(e.toString());
+    }
+  }
+
+  Future<Either<String, GmapModel>> getPosition({
+    required double lat,
+    required double long,
+  }) async {
+    final Location location = Location();
+    late LocationData locationData;
+    final state = await _getPermission(location);
+
+    if (!state) return const Left("Permission not granted");
+    locationData = await location.getLocation();
+    final latLng = LatLng(
+      locationData.latitude!,
+      locationData.longitude!,
+    );
+
+    final info = await geo.placemarkFromCoordinates(
+      lat,
+      long,
+    );
+
+    String? address;
+    if (info.isNotEmpty) {
+      final place = info[0];
+      address =
+          '$place.street, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    } else {
+      address = 'Location not found';
+    }
+
+    return Right(GmapModel(latLng: latLng, address: address));
+  }
+}
